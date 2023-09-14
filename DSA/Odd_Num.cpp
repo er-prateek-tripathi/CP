6ef79fdf662623805afecdd8565c4fbe5f360856
Predator <prateek772492@gmail.com>
2023-09-14 19:37:22 +0530

    Odd Numbers via loops.

---
+++ DSA/Odd_Num.cpp
@@ -0,0 +1,22 @@
+// The task is to print all the odd numbers from 1 to n.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+    int n;
+
+    cout<<endl<<"Enter the value of n."<<endl;
+
+    cin>>n;
+
+    for(int i = 1; i<=n; i++) {
+        if (i%2 != 0)
+            cout<<i<<endl;
+    }
+    
+    return 0;
+}
