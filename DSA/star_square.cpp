03c10c576f5b7904cf768a6c113a9c4d23b055b0
Predator <prateek772492@gmail.com>
2023-10-04 00:33:34 +0530

    pattern programming

---
+++ DSA/star_square.cpp
@@ -0,0 +1,22 @@
+/*This code will make the following pattern.
+* * * *
+* * * * 
+* * * * 
+* * * *
+the dimension of square depends on the user.
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int i = 1;
+    while ( i<=n ) {
+
+    }
+
+    
+return 0;
+}
