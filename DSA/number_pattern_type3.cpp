bf0785e083256b8d1e1fdeee9086f60a37630136
PREDATOR <prateek772492@gmail.com>
2023-10-21 12:12:31 +0530

    pattern

---
+++ DSA/number_pattern_type3.cpp
@@ -0,0 +1,30 @@
+/*
+1
+2 1
+3 2 1
+4 3 2 1
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+system("cls"); 
+
+int count = 1, i= i ;
+int n; 
+cin >> n;
+
+while (i<=n) {
+    int j = 1, value = i;
+    while(j<=i) {
+        cout<<value<<" ";
+        value -= 1;
+        j += 1;
+
+    }
+    cout<<endl;
+    i += 1;
+}
+
+return 0;
+}
