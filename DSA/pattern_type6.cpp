d052dac347445fc5dbb3e3185fea64bd0e41ce44
PREDATOR <prateek772492@gmail.com>
2023-10-21 13:07:28 +0530

    pattern

---
+++ DSA/pattern_type6.cpp
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+system("cls"); 
+int n = 4;
+int i = 1;
+while (i<=n){
+    int j = 1;
+    char ch = 'A' + i - 1;
+    while(j<=i){
+        cout<<ch<<" ";
+        j+=1;
+    }
+    cout<<endl;
+    i+=1;
+}
+
+return 0;
+}
