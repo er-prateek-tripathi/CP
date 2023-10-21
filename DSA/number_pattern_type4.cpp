086a8058623d8003fda24d29331df3e4d997987e
PREDATOR <prateek772492@gmail.com>
2023-10-21 12:47:18 +0530

    pattern

--- DSA/number_pattern_type4.cpp
+++ DSA/number_pattern_type4.cpp
@@ -1,4 +1,8 @@
 /*
+n = 3
+A B C
+D E F
+G H I
 
 */
 #include<bits/stdc++.h>
@@ -6,9 +10,21 @@ using namespace std;
 
 int main() {
 system("cls"); 
-int n;
-cin>>n;
+int n = 3;
+//cin>>n;
+int i = 1;
+char ch = 'A';
+while(i<=n){
+   int j = 1;
+   while (j<=n){
+    cout<<ch<<" ";
+    ch += 1;
+    j += 1;
+   }
+   cout<<endl;
+   i += 1;
 
+}
 
 return 0;
 }
