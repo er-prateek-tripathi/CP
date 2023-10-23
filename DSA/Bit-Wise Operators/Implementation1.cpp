05f38575e2cc37946cdbe8846eb1e37626783a6c
PREDATOR <prateek772492@gmail.com>
2023-10-23 19:51:07 +0530

    Bitwise

---
+++ DSA/Bit-Wise Operators/Implementation1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+system("cls"); 
+int a = 4, b = 6;
+
+cout<<"4 AND 6 = 4&6 = " <<(a&b)<<endl;
+
+cout<<"4 OR 6 = 4|6 = "<<(a|b)<<endl;
+
+cout<<" ~4 = "<<~a<<" and ~6 = "<<~b<<endl;
+
+cout<<"4 ^ 6 = 4^6 = "<<(a^b)<<endl;
+
+cout<<"5<<1 = "<<(5<<1)<<endl;
+
+cout<<"3<<2 = "<<(3<<2)<<endl;
+
+cout<<"5>>2 = "<<(5>>2)<<endl;
+
+cout<<"15>>1 = "<<(15>>1)<<endl;
+
+cout<<"20>>1 = "<<(20>>1)<<endl;
+
+cout<<"19<<1 = "<<(19<<1)<<endl;
+
+cout<<"21<<2 = "<<(21<<2)<<endl;
+
+
+
+
+return 0;
+}
