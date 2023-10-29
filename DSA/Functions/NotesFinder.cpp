ee5adad03f88639cfc9fb2e519ef95c7de3ebf15
PREDATOR <prateek772492@gmail.com>
2023-10-29 14:32:45 +0530

    Work in Progress

---
+++ DSA/Functions/NotesFinder.cpp
@@ -0,0 +1,28 @@
+/* This program will tell you how many number of notes 
+of a specific kind of required to equal to a certain amount.
+
+Example: 
+Sum =  1330
+100 rupee notes: 1330/100 = 13
+50 rupee notes: 1330 - 13*100 = 30
+                30/50 = 0(int value)
+20 rupee note: 1330 - 13*100 = 30
+                30/20 = 1
+1 rupee note = 1330 - 13*100 - 20*1 = 10
+                10/1 = 10
+
+So we need 13 100 rupee notes, 1 20 rupee note and 10 1 rupee note.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int amount;
+    cout << "Enter the amount: " << endl;
+    cin >> amount;
+
+     
+return 0;
+}
