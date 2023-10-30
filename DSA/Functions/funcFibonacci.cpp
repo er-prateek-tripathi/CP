09f072475e26ca00618b83d66f47b69ac3c57824
PREDATOR <prateek772492@gmail.com>
2023-10-30 18:22:56 +0530

    Nth term of Fibonacci Series.

--- DSA/Functions/funcFibonacci.cpp
+++ DSA/Functions/funcFibonacci.cpp
@@ -1,1 +1,23 @@
-// A unction to print fibonacci series.
+// A function to print nth term of fibonacci series.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int FibSeries ( int n ) {
+    int a = 0, b = 1, ans = 0;
+    for (int i = 0; i<n; i++){
+        int next = a+b;
+        a = b; 
+        b = next;
+        ans = next;
+        }
+    
+    return ans;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<FibSeries(n);
+return 0;
+}
