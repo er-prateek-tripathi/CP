b8a8209667c8a9f6345f11673b853cca8b53fcdd
PREDATOR <prateek772492@gmail.com>
2023-11-08 16:01:48 +0530

    Intersection of two arrays.

--- DSA/Arrays/Intersection.cpp
+++ DSA/Arrays/Intersection.cpp
@@ -3,34 +3,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void input (int arr[], int arr1[], int n) {
+void input (vector <int> &arr, vector <int> &arr1, int n, int m) {
 
     cout << "Enter the elements for the First Array: " << endl;
+    int input;
     for ( int i = 0; i < n; i++ ){
-        cin >> arr[i];
+        cin >> input;
+        arr.push_back(input);
     }
 
     cout << "Enter the elements for the Second Array: " << endl;
-    for ( int i = 0; i < n; i++ ){
-        cin >> arr1[i];
+    int input1;
+    for ( int i = 0; i < m; i++ ){
+        cin >> input1;
+        arr1.push_back(input1);
     }
+
 
 }
 
-void Intersection ( int arr[], int arr1[], int n) {
-    //Incomplete
+vector <int> Intersection ( vector <int> &arr, vector <int> &arr1, int n, int m) {
+    sort(arr.begin(), arr.end());
+    sort(arr1.begin(), arr1.end());
+
+    vector <int> ans;
+
+    for ( int i = 0; i < n; i++ ) {
+        int ele = arr[i];
+
+        for ( int j = 0; j < m; j++ ) {
+            if ( ele == arr1[j] ) {
+                ans.push_back(ele);
+                arr1[j] = -1;
+                break;
+            }
+        }
+    }
+    return ans;
 }
 
 int main() {
 
-    int arr[1000], arr1[1000];
-    int n;
+    vector <int> arr;
+    vector <int> arr1;
+    int n, m;
  
     cout << "No. of elements: ";
-    cin >> n;
+    cin >> n >> m;
 
-    input ( arr, arr1, n);
-    Intersection ( arr, arr1, n );
+    input ( arr, arr1, n, m );
+    vector <int> ans = Intersection ( arr, arr1, n, m );
 
+    for (int i: ans) {
+        cout << i;
+    }
 return 0;
 }
