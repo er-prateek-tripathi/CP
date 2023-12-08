113f42e9a1957c39805903e2918cede42efefded
PREDATOR <prateek772492@gmail.com>
2023-12-08 13:50:21 +0530

    Square root using Binary Search

--- DSA/Binary Search/SquareRoot.cpp
+++ DSA/Binary Search/SquareRoot.cpp
@@ -10,7 +10,7 @@ Ideology:
 #include<bits/stdc++.h>
 using namespace std;
 
-long long int BS( int n ) {
+long long int sqrt( int n ) {
     int s = 0;
     int e = n;
 
@@ -35,14 +35,27 @@ long long int BS( int n ) {
         
     }
     return ans;
-
 }
 
-int sqrt(int x){
-    return BS(x);
+double morePrecision( int n, int precision, int tempSol ){
+    double factor = 1;
+    double ans  = tempSol;
+
+    for ( int i = 0; i < precision; i++) {
+        factor = factor/10;
+        for ( double j = ans; j*j < n; j = j + factor ) {
+            ans = j;
+        }
+    } 
+    return ans;
 }
 
 int main() {
-    cout<<sqrt(36);
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    int tempSol = sqrt(n);
+
+    cout << "Square Root: " << morePrecision( n, 3, tempSol);
 return 0;
 }
